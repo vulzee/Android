049fe1e3e66ecad3457e6a1b0f99058b20c71d4e
woloszp <wolosz.patryk@gmail.com>
2017-04-26 16:21:31 +0200

    login view + show search view beta

--- Serializer/app/src/main/java/serializerteam/serializer/ShowSearchFragment.java
+++ Serializer/app/src/main/java/serializerteam/serializer/ShowSearchFragment.java
@@ -6,14 +6,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ShowSearchFragment extends Fragment {
+
+    private String spinnerCategories[];
+    private Spinner genreSpinner;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_shows, container, false);
-
+        spinnerCategories=new String[5];
+        spinnerCategories[0]="genre 1";
+        spinnerCategories[1]="genre 2";
+        spinnerCategories[2]="genre 3";
+        spinnerCategories[3]="genre 4";
+        spinnerCategories[4]="genre 5";
+        genreSpinner = (Spinner) view.findViewById(R.id.genre_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, spinnerCategories);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        genreSpinner.setAdapter(adapter);
         return view;
     }
 }
