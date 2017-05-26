9013130a2afa73b8b14d112aa8d8b658ca28de72
ipienkowski <ipienkowski@pl.sii.eu>
2017-05-26 21:28:19 +0200

    cgw

---
+++ Serializer/app/src/main/java/serializerteam/serializer/adapter/WidgetService.java
@@ -0,0 +1,97 @@
+package serializerteam.serializer.adapter;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import serializerteam.serializer.R;
+import serializerteam.serializer.dto.ShowDto;
+
+/**
+ * Created by xxx on 2017-05-26.
+ */
+
+public class WidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new ListRemoteViewsFactory(this.getApplicationContext(), intent);
+    }
+}
+
+class ListRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    private static final int mCount = 10;
+    private List<String> mWidgetItems = new ArrayList<>();
+    private Context mContext;
+    private int mAppWidgetId;
+
+    public ListRemoteViewsFactory(Context context, Intent intent) {
+        mContext = context;
+        mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+
+    public void onCreate() {
+        // In onCreate() you setup any connections / cursors to your data source. Heavy lifting,
+        // for example downloading or creating content etc, should be deferred to onDataSetChanged()
+        // or getViewAt(). Taking more than 20 seconds in this call will result in an ANR.
+        for (int i = 0; i < mCount; i++) {
+            mWidgetItems.add(i + "!");
+        }
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    public RemoteViews getViewAt(int position) {
+
+        // Construct a remote views item based on the app widget item XML file,
+        // and set the text based on the position.
+        RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_next_show_item);
+        rv.setTextViewText(R.id.widget_next_episode, mWidgetItems.get(position)+ "time");
+        rv.setTextViewText(R.id.appwidget_text, mWidgetItems.get(position) );
+
+        // Return the remote views object.
+        return rv;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+}
