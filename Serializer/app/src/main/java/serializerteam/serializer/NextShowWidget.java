01d461dddba56fb68a730a003c0ddea493170a7e
ipienkowski <ipienkowski@pl.sii.eu>
2017-05-26 21:37:46 +0200

    aaaa

--- Serializer/app/src/main/java/serializerteam/serializer/NextShowWidget.java
+++ Serializer/app/src/main/java/serializerteam/serializer/NextShowWidget.java
@@ -80,9 +80,10 @@ public class NextShowWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+       // for (int appWidgetId : appWidgetIds) {
+            updateWidgetData(context,appWidgetIds,appWidgetManager);
+            //updateAppWidget(context, appWidgetManager, appWidgetId);
+       // }
     }
 
     @Override
